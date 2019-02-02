884c2653c1618517b1e0be68d67517b6ece47721
Martin Nicholson <recyclebing+github@gmail.com>
2019-02-02 16:15:18 +0100

    Fetch last commit author & time on any branch (async/await)

--- repoScraper.js
+++ repoScraper.js
@@ -1,5 +1,8 @@
 /* eslint-env: node, es6 */
 
+const timeAgo = require('date-fns/distance_in_words_to_now');
+const daysDiff = require('date-fns/difference_in_days');
+const parseDate = require('date-fns/parse');
 const github = require('octonode');
 const config = require('./config.json');
 const token = require('./token.json').gh_token;
@@ -13,7 +16,7 @@ var client = github.client(token);
  * @param {String} filename
  * @returns {String} file content
  */
-async function getFileContents(ghrepo, branch, filename = 'manifest.php') {
+async function getFileContents(ghrepo, branch = 'master', filename = 'manifest.php') {
     var results = await ghrepo.contentsAsync(filename, branch);
     return Buffer.from(results[0].content, 'base64').toString();
 }
@@ -29,6 +32,72 @@ function extractVersion(manifest) {
     return (results && results.length > 1) ? results[1] : 'unknown';
 }
 
+async function getVersion(ghrepo, branch = 'master') {
+    var toTry = ['manifest.php', 'package.json'];
+    var version = await getFileContents(gherpo, branch, toTry[0]).then(extractVersion);
+    return version;
+}
+
+/**
+ * Retrieve last commit & author info from a branch
+ *
+ * @param {Object} ghrepo
+ * @param {String} branch
+ * @returns {Object} - relevant info about the commit
+ */
+async function getLastCommitOnBranch(ghrepo, branch = 'master') {
+    var branch = await ghrepo.branchAsync(branch);
+    //console.log('<b>', branch[0].commit, '</b>');
+    if (branch[0] && branch[0].commit) {
+        //console.log('<c>', commit, '</c>');
+        return {
+            author: branch[0].commit.author.login,
+            date: timeAgo(parseDate(branch[0].commit.commit.author.date)),
+            sha: branch[0].commit.sha.slice(0,7)
+        };
+    }
+    return null;
+}
+
+/**
+ * Reports the number of open PRs in a repo that were updated recently
+ *
+ * @param {Object} ghrepo
+ * @param {String} branch
+ * @param {Number} days - how old a PR to consider
+ * @returns {Promise<Number>} - resolves with the number of open PRs touched in last N days
+ */
+async function getRecentPRCount(ghrepo, branch = 'master', days = 70) {
+    var prs = await ghrepo.prsAsync();
+    return prs[0].filter(pr => {
+        // restrict by state & date:
+        var diff = Math.abs(daysDiff(Date.now(), parseDate(pr.updated_at)));
+        // console.log(pr.state);
+        // console.log(diff);
+        return pr.state === 'open';// && diff < days;
+    }).length;
+}
+
+/**
+ * Reports whether branch1 is ahead, behind or equal with branch2
+ *
+ * @param {Object} ghrepo
+ * @param {String} branch1
+ * @param {String} branch2
+ * @returns {Promise<String>} - resolves with status description
+ */
+async function getComparison(ghrepo, branch1 = 'master', branch2 = 'develop') {
+    var comp = await ghrepo.compareAsync(branch1, branch2);
+    comp = comp[0];
+    var status = (comp.status === 'identical') ? `equal with`
+    : (comp.ahead_by) ? `${comp.ahead_by} commits ahead of`
+    : (comp.behind_by) ? `${comp.behind} commits behind`
+    : `??? to`;
+    // console.log(status);
+    return `${branch1} is ${status} ${branch2}`
+}
+
+
 /**
  * Fetch details about a repo on Github
  *
@@ -40,15 +109,34 @@ function fetch(repoNameFull) {
         var ghrepo = client.repo(repoNameFull);
         if (!ghrepo) return {};
 
-        var masterVersion = getFileContents(ghrepo, 'master').then(extractVersion).catch(console.err);
-        var developVersion = getFileContents(ghrepo, 'develop').then(extractVersion).catch(console.err);
-    
-        return Promise.all([masterVersion, developVersion])
-            .then(versions => {
+        // if master exists:
+        var masterVersion = getFileContents(ghrepo, 'master').then(extractVersion);
+        var masterLast = getLastCommitOnBranch(ghrepo, 'master');
+        // if develop exists:
+        var developVersion = getFileContents(ghrepo, 'develop').then(extractVersion);
+        var developLast = getLastCommitOnBranch(ghrepo, 'develop');
+        // extra:
+        var recentPRs = getRecentPRCount(ghrepo);
+        var status = getComparison(ghrepo);
+
+        return Promise.all([masterVersion, developVersion, masterLast, developLast, recentPRs, status])
+            .then(([masterVersion, developVersion, masterLast, developLast, recentPRs, status]) => {
                 return {
                     name: repoNameFull,
-                    master: versions[0],
-                    develop: versions[1]
+                    master: {
+                        version: masterVersion || '?',
+                        lastAuthor: masterLast.author || 'unknown',
+                        lastDate: masterLast.date || 'unknown',
+                        lastSHA: masterLast.sha || 'unknown'
+                    },
+                    develop: {
+                        version: developVersion || '?',
+                        lastAuthor: developLast.author || 'unknown',
+                        lastDate: developLast.date || 'unknown',
+                        lastSHA: developLast.sha || 'unknown'
+                    },
+                    recentPRs: recentPRs || 0,
+                    status: status
                 };
             })
             .then(resolve)
@@ -59,9 +147,9 @@ function fetch(repoNameFull) {
     });
 }
 
-/** 
+/**
  * Flattens a JSON structure to allow multiple orgs & repos to be processed as a flat array
- * 
+ *
  * @param {Object} json - typically direct from config file
  * @returns {Array}
  */
@@ -71,9 +159,7 @@ function flatten(json) {
     }));
 }
 
-//console.log(flatten(config.enabled));
-
-/** 
+/**
  * Start fetching repos data!
  */
 module.exports.fetchAll = function fetchAll() {
@@ -82,3 +168,8 @@ module.exports.fetchAll = function fetchAll() {
         .map(fetch)
     );
 };
+
+module.exports.getApiLeft = async function getApiLeft() {
+    var limit = await client.limitAsync();
+    return limit[0];
+}
